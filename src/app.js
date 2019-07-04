7c75b9e7d7cd956ff787142dd87f5a31b801a3c1
Madeline Carr <madelinencarr@Gmail.com>
2019-07-04 01:37:04 -0400

    Reset Body Styles

--- src/app.js
+++ src/app.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Child from './js/childComponent';
 import Header from './js/header';
 import ImagePage from './js/imagePage';
 import Footer from './js/footer';
 
-const title = 'React is Working!';
 const App = () => {
     return (
         <div>
             <Header />
             <ImagePage />
-            <h1>{title}</h1>
-            <Child />
-
             <Footer />
         </div>
     );
