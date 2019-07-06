60cf3013e0b04b4e8d88e31e56700f6f9739af98
Madeline Carr <madelinencarr@Gmail.com>
2019-07-06 04:32:04 -0400

    Added Animals Page

---
+++ src/js/animals.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const ImagePage = () => {
+    return (
+        <div className="wrapper">
+            <div className="gridThree">
+                <div className="imgBox">
+                    <img src="./images/TigerSq.jpg" alt="" />
+                    <h2>Tiger</h2>
+                    <h3>2018</h3>
+                    <p>This is a illustration of a tiger.</p>
+                </div>
+                <div className="imgBox">
+                    <img src="./images/TigerSq.png" alt="" />
+                    <h2>Tiger</h2>
+                    <p>This is a illustration of a tiger.</p>
+                </div>
+                <div className="imgBox">
+                    <img src="./images/TigerSq.jpg" alt="" />
+                    <h2>Tiger</h2>
+                    <p>This is a illustration of a tiger.</p>
+                </div>
+                <div className="imgBox">
+                    <img src="./images/TigerSq.png" alt="" />
+                    <h2>Tiger</h2>
+                    <p>This is a illustration of a tiger.</p>
+                </div>
+                <div className="imgBox">
+                    <img src="./images/TigerSq.jpg" alt="" />
+                    <h2>Tiger</h2>
+                    <p>This is a illustration of a tiger.</p>
+                </div>
+                <div className="imgBox">
+                    <img src="./images/TigerSq.jpg" alt="" />
+                    <h2>Tiger</h2>
+                    <p>This is a illustration of a tiger.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ImagePage;
