814eea287d0064e1f3dbc344d3675035a3329ebb
Madeline Carr <madelinencarr@Gmail.com>
2019-07-06 04:32:13 -0400

    Added Routing Base

--- src/app.js
+++ src/app.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import Header from './js/header';
 import ImagePage from './js/imagePage';
 import Footer from './js/footer';
+import Home from './js/home';
 
 const App = () => {
     return (
-        <div>
-            <Header />
-            <ImagePage />
-            <Footer />
-        </div>
+        <React.Fragment>
+            <BrowserRouter>
+                <Header />
+                <main className="content">
+                    <Switch>
+                        <Route path="/image-page" component={Home} />
+                        <Route path="/about-us" component={Home} />
+                        <Route path="/contact" component={Home} />
+                        <Route path="/menu" component={Home} />
+                        <Route path="/home" component={Home} />
+                        <Redirect exact from="/" to="/home" />
+                        <Redirect to="/home" />
+                        <Route exact path="/" component={Home} />
+                    </Switch>
+                </main>
+                <Footer />
+            </BrowserRouter>
+        </React.Fragment>
     );
 };
 
---
+++ src/js/home.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="wrapper">
+            <div className="gridThree">
+                <div className="imgBox">
+                    <img src="./images/TigerSq.jpg" alt="" />
+                    <h2>Tiger</h2>
+                    <h3>2018</h3>
+                    <p>This is a illustration of a tiger.</p>
+                </div>
+                <div className="imgBox">
+                    <img src="./images/Night.png" alt="" />
+                    <h2>Tiger</h2>
+                    <p>This is a illustration of a tiger.</p>
+                </div>
+                <div className="imgBox">
+                    <img src="./images/Boom.jpg" alt="" />
+                    <h2>Tiger</h2>
+                    <p>This is a illustration of a tiger.</p>
+                </div>
+                <div className="imgBox">
+                    <img src="./images/Wolf.png" alt="" />
+                    <h2>Tiger</h2>
+                    <p>This is a illustration of a tiger.</p>
+                </div>
+                <div className="imgBox">
+                    <img src="./images/TigerSq.jpg" alt="" />
+                    <h2>Tiger</h2>
+                    <p>This is a illustration of a tiger.</p>
+                </div>
+                <div className="imgBox">
+                    <img src="./images/TigerSq.jpg" alt="" />
+                    <h2>Tiger</h2>
+                    <p>This is a illustration of a tiger.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
