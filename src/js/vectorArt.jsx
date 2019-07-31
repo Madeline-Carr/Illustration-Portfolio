b6dd9e40b915784657878bfc6ca7683cb897af0b
Madeline Carr <madelinencarr@Gmail.com>
2019-07-30 23:48:15 -0400

    Created Vector Art Page

---
+++ src/js/vectorArt.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Animals = () => {
+    return (
+        <div className="wrapper">
+            <div className="gridThree">
+                <div className="imgBox">
+                    <img
+                        src="./images/Animals/Tiger.png"
+                        className="topicImage"
+                        alt="Tiger"
+                    />
+                    <h2>Placeholder</h2>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Animals;
