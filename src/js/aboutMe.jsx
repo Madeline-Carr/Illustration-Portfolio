7e7bf46274da65c21f6743c7aa840af522420763
Madeline Carr <madelinencarr@Gmail.com>
2019-08-07 23:11:15 -0400

    Update About Me

--- src/js/aboutMe.jsx
+++ src/js/aboutMe.jsx
@@ -13,14 +13,15 @@ const AboutMe = () => {
                 </div>
                 <div className="imgBox">
                     <h2>About Me</h2>
+                    <p> Hello!</p>
                     <p>
-                        Hello! My name is Madeline Carr, I am a graduate of
-                        Virgina Commonwealth University. I like to experiment
-                        with different styles of illustration in order to
-                        improve my understanding of the world. Most of my work
-                        is vector illustrations which I like because it helps me
-                        think about shapes. I am working to become a web
-                        developer. I hope you enjoy my portfolio!
+                        My name is Madeline Carr, I am a graduate of Virgina
+                        Commonwealth University. I like to experiment with
+                        different styles of illustration in order to improve my
+                        understanding of the world. Most of my work is vector
+                        illustrations which I like because it helps me think
+                        about shapes. I am working to become a web developer. I
+                        hope you enjoy my portfolio!
                     </p>
                 </div>
             </div>
